7ae5a19f95012987d455562ca443fd82375c91a1
Pablo Melo Mac <panmecar@gmail.com>
2026-03-02 22:56:19 -0500

    feat(ui):add splash screen

---
+++ android/app/src/main/kotlin/com/example/pokeglobal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pokeglobal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:pokeglobal/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PokeGlobal',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
